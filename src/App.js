2338bb0514ca238405a47bd7496679dadd20e5a1
alec <abacqu0716@gmail.com>
2023-03-27 16:25:42 -0500

    initial routing

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<div>login page</div>} />
+      </Route>
+      
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://www.nbcsportsedge.com/sites/default/files/styles/hero_secondary_desktop_lg/public/2022-11/KelceFMIA_USA.webp?itok=c2JvbrbZ" alt="chiefs"/>
+             </div>
+            <div className="texts">
+                <h2>Chiefs offseason begins now</h2>
+                <p className="info">
+                    <a className="author">Abacqu</a>
+                    <time>2023-03-10 12:45</time>
+                </p>
+                <p className="summary">With the superbowl over, plans for the new season start now.</p>
+            </div>
+      </div> 
+    )
+}
